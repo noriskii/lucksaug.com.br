17d3d4aa7f61e32518ffbfc439fdcb36a96bb171
Lucas Augusto <luckaugusto@gmail.com>
2020-10-30 19:39:24 -0300

    organizing some folders and files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lucksaug Personal Blog`,
+    description: `I write about anything i love, most part of the time, it's computers, games and life`,
+    author: `@lucksaug`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
