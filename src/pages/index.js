0cc1625bb867ebdc39366fc17b5efa9fc7cf5b81
Lucas Augusto <luckaugusto@gmail.com>
2020-11-11 10:57:07 -0300

    create about page and link to it on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,7 +7,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>My Blog</h1>
+    <h1>Home</h1>
+    <ul>
+      <li>
+        <Link to="/about" activeStyle={{ color: "red" }}>
+          About
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
