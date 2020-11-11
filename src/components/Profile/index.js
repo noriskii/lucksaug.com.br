1c23647c057e36e25ce2af3f7b12ca0d4c648311
Lucas Augusto <luckaugusto@gmail.com>
2020-11-11 10:57:36 -0300

    Creating Profile Component and add aside to Layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Lucas Augusto Silva</h1>
+    <h2>Desenvolvedor FullStack</h2>
+    <p>
+      Membro do time de Ferramentas da Ericsson Inovação, escrevo um pouquinho
+      sobre programação e sobre lifestyle em geral (posso arriscar um pouquinho
+      a falar de jogos)
+    </p>
+  </div>
+)
+
+export default Profile
